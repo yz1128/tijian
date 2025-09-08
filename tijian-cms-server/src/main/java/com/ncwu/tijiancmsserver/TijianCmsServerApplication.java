11fddb22f339e5a29c6c7d5a23e1d6a4047c3bdb
Yanz <871022082@qq.com>
2025-09-08 11:45:21 +0800

    test

---
+++ tijian-cms-server/src/main/java/com/ncwu/tijiancmsserver/TijianCmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.ncwu.tijiancmsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TijianCmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TijianCmsServerApplication.class, args);
+    }
+
+}
---
+++ tijian-cms-server/src/test/java/com/ncwu/tijiancmsserver/TijianCmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncwu.tijiancmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TijianCmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
